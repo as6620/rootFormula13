6f346e931841ff19a191e61c07c25652f0381d72
as6620 <as6620@bs.amalnet.k12.il>
2024-04-08 23:18:08 +0300

    random onClick

--- app/src/main/java/com/example/rootformula13/MainActivity.java
+++ app/src/main/java/com/example/rootformula13/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rootformula13;
 
+import android.content.Intent;
 import  android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
--- app/src/main/java/com/example/rootformula13/solActivity.java
+++ app/src/main/java/com/example/rootformula13/solActivity.java
@@ -14,7 +14,7 @@ public class solActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sol);
+
     }
 
-    
 }
